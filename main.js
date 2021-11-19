5d915d3cc3e2ae67ae3ec7081d211e64fa73de6a
Fredrik Alm <Fredrik_alm@outlook.com>
2021-11-19 15:39:59 +0100

    uppdateringar

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
 // define variable for ball count paragraph
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+        if (!event.target.matches('.dropbtn')) {
+            var dropdowns = document.getElementsByClassName("dropdown-content");
+            var i;
+            for (i = 0; i < dropdowns.length; i++) {
+                var openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                }
+            }
+        }
+    }
+    //efter
 
 const para = document.querySelector('p');
 let count = 0;
-let antal = 14;
+let antal = 0;
 
 function restart() {
     location.reload();
